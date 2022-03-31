1276b28d194b9eac53d2ec6ba17f5b957b6b5e8d
noori <noori@binshops.com>
2022-03-31 20:08:07 +0430

    fix bug

--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 const express =require('express');
-
 const app=express();
 
+app.use(express.static(__dirname + "/../dist"));
+
 app.get('*',(req,res)=>{
-    if (process.env.NODE_ENV !=='production'){
-        return res.send('running server in development mode!');
-    } else {
-        return res.sendFile('index.html',{root:__dirname+'/../dist/'})
-    }
+        return res.sendFile('index.html', {root:__dirname + '/../dist/'})
 })
 app.listen(80,()=>{
     console.log('app running on port 80')
