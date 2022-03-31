b3831b8f29cbbbfece460abebc257c3c18cd3de8
noori <noori@binshops.com>
2022-03-31 19:25:03 +0430

    server added

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app=express();
+
+app.get('*',(req,res)=>{
+    if (process.env.NODE_ENV !=='production'){
+        return res.send('running server in development mode!');
+    } else {
+        return res.sendFile('index.html',{root:__dirname+'/../dist/'})
+    }
+})
+app.listen(80,()=>{
+    console.log('app running on port 80')
+});
+
+module.exports=app;
